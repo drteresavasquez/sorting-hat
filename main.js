13556d319870e84076340553507bfc7b58779128
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-08 12:25:06 -0600

    remove button from DOM

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const studentArray = [];
 const voldermortsArmy = [];
 const houseColors = {
-    gryffindor: "#7F0909",
-    slytherin: "#0D6217",
-    hufflepuff: "#EEE117",
-    ravenclaw: "#000A90"
+  gryffindor: '#7F0909',
+  slytherin: '#0D6217',
+  hufflepuff: '#EEE117',
+  ravenclaw: '#000A90',
 };
 
 const domEvents = () => {
-  document.querySelector("#start-sorting").addEventListener("click", showForm);
-  document.querySelector("#sorting-form").addEventListener("click", addStudentToArray);
-  document.querySelector('#first-years-card-display').addEventListener("click", expelStudent);
+  document.querySelector('#start-sorting').addEventListener('click', showForm);
+  document
+    .querySelector('#sorting-form')
+    .addEventListener('click', addStudentToArray);
+  document
+    .querySelector('#first-years-card-display')
+    .addEventListener('click', expelStudent);
 };
 
 const renderToDOM = (divId, content) => {
@@ -19,6 +23,7 @@ const renderToDOM = (divId, content) => {
 };
 
 const showForm = () => {
+  document.querySelector('#button-div').innerHTML = '';
   const content = `<div class="card m-3 justify-content-center">
                         <div class="card-body text-center">  
                             <h4>
@@ -33,89 +38,102 @@ const showForm = () => {
                         </div>
                     </div>`;
 
-  renderToDOM("#sorting-form", content);
+  renderToDOM('#sorting-form', content);
 };
 
 const addStudentToArray = (e) => {
-  if (e.target.id === "sort") {
-    const student = document.querySelector("#student-name");
+  if (e.target.id === 'sort') {
+    const student = document.querySelector('#student-name');
     errorMessage(student.value);
 
     if (student.value) {
       studentArray.push({
         name: student.value,
-        house: sortingHat()
+        house: sortingHat(),
       });
-      student.value = "";
+      student.value = '';
     }
 
     let sortedArray = sortStudentsByHouse(studentArray);
-    cardCreator("#first-years-card-display", sortedArray);
+    cardCreator('#first-years-card-display', sortedArray);
   }
 };
 
 const sortingHat = () => {
-  const houses = ["gryffindor", "slytherin", "hufflepuff", "ravenclaw"];
+  const houses = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw'];
   let sortingHat = houses[Math.floor(Math.random() * houses.length)];
   return sortingHat;
 };
 
 const sortStudentsByHouse = (array) => {
-    return array.sort((a, b) => (a.house > b.house) ? 1 : (a.house === b.house) ? ((a.name > b.name) ? 1 : -1) : -1 )
-}
+  return array.sort((a, b) =>
+    a.house > b.house
+      ? 1
+      : a.house === b.house
+      ? a.name > b.name
+        ? 1
+        : -1
+      : -1
+  );
+};
 
 const cardCreator = (divId, array) => {
-  let card = "";
+  let card = '';
   for (let i = 0; i < array.length; i++) {
-    if(divId.includes('voldermort')) {
-        card += `<div class="card m-3" style="width: 18rem;">
+    if (divId.includes('voldermort')) {
+      card += `<div class="card m-3" style="width: 18rem;">
                     <img class="card-img-top" src="https://vignette.wikia.nocookie.net/harrypotter/images/d/d4/Death_Eaters_WBST.png/revision/latest?cb=20161205041948" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Sadly, <b>${array[i].name}</b> went over to the dark side!</p>
                     </div>
                 </div>`;
     } else {
-        card += `<div class="card m-3" style="min-width: 300px;" id="${i}">
+      card += `<div class="card m-3" style="min-width: 300px;" id="${i}">
                     <div class="row no-gutters">
-                        <div class="col-md-4" style="min-height: 150px; background-color: ${houseColors[array[i].house]}">
+                        <div class="col-md-4" style="min-height: 150px; background-color: ${
+                          houseColors[array[i].house]
+                        }">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title">${array[i].name}</h5>
-                                <p class="card-text">${(array[i].house).toUpperCase()}</p>
+                                <p class="card-text">${array[
+                                  i
+                                ].house.toUpperCase()}</p>
                                 <button type="button" id="${i}" class="btn btn-danger">EXPEL</button>
                             </div>
                         </div>
                     </div>
                 </div>`;
-      }
+    }
   }
-  
+
   renderToDOM(divId, card);
 };
 
 const errorMessage = (student) => {
   if (student) {
-      document.querySelector("#error-message").innerHTML = "";
-} else {
-    document.querySelector("#error-message").innerHTML = 
-        `<div style="color: red;margin-bottom: 10px;">
+    document.querySelector('#error-message').innerHTML = '';
+  } else {
+    document.querySelector(
+      '#error-message'
+    ).innerHTML = `<div style="color: red;margin-bottom: 10px;">
             <b>Please type a name</b>
         </div>`;
   }
 };
 
 const expelStudent = (e) => {
-    if (e.target.type === 'button') {
-        voldermortsArmy.push(studentArray[e.target.id])
-        studentArray.splice(e.target.id, 1)
-        cardCreator("#first-years-card-display", studentArray);
-        cardCreator("#voldermorts-army", voldermortsArmy);
-    }
-}
+  if (e.target.type === 'button') {
+    voldermortsArmy.push(studentArray[e.target.id]);
+    studentArray.splice(e.target.id, 1);
+    cardCreator('#first-years-card-display', studentArray);
+    cardCreator('#voldermorts-army', voldermortsArmy);
+  }
+};
 
 const init = () => {
-    domEvents();
+  domEvents();
 };
 
 init();
